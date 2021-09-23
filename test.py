9ee2f7414523111e9a894cd3e6ba5644406d124d
Gino-Y <ginoyan250@gmail.com>
2021-09-23 16:55:48 +0800

    add

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-import tensorflow
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('./PRSA_data_2010.1.1-2014.12.31.csv')
+data.info()
